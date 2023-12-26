5499b3913e98e3529e263967d3dbb2cece0a38d7
Preet Kothari <preetkothari19@gmail.com>
2023-12-26 13:58:25 +0530

    Update quiz.py

--- Day-9/Multiple-Linear-Regression/quiz.py
+++ Day-9/Multiple-Linear-Regression/quiz.py
@@ -1,25 +1,21 @@
-# Multiple Linear Regression
-# Reference: https://www.hackerrank.com/challenges/s10-multiple-linear-regression/tutorial
-
-# Question: https://www.hackerrank.com/challenges/s10-multiple-linear-regression
-
 # Import library
 from sklearn import linear_model
 
 # Set data
-m, n = map(int, input().split())
-X, Y = [], []
+value = list(map(int,input().split()))
+m = value[0]
+n = value[1]
+X = []
+Y = []
 
-# Get the parameters X and Y for discovery the variables a and b
+# Get the parameters X and Y for discovering variables a and b
 for i in range(n):
     x = [0]
-    elements = list(map(float, input().split()))
-    for j in range(len(elements)):
-        if j < m:
-            x.append(elements[j])
-        else:
-            Y.append(elements[j])
+    elements = list(map(float,input().split()))
+    for j in range(m):
+        x.append(elements[j])
     X.append(x)
+    Y.append(elements[m])
 
 # Set the model LinearRegression
 model = linear_model.LinearRegression()
@@ -27,17 +23,17 @@ model.fit(X, Y)
 a = model.intercept_
 b = model.coef_
 
-# Get the parameters X for discovery the Y
+# Get the parameters X for discovering the Y
 q = int(input())
 new_X = []
 for i in range(q):
     x = [0]
     elements = list(map(float, input().split()))
-    for j in range(len(elements)):
+    for j in range(m):
         x.append(elements[j])
     new_X.append(x)
 
-# Gets the result and show on the screen
+# Gets the result and show it on the screen
 result = model.predict(new_X)
 for i in range(len(result)):
     print(round(result[i],2))
