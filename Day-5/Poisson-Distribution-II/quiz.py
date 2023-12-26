86b44422987b1a6c3020bf9ed2bc1e25d119ace6
Preet Kothari <preetkothari19@gmail.com>
2023-12-26 13:24:05 +0530

    Update quiz.py

--- Day-5/Poisson-Distribution-II/quiz.py
+++ Day-5/Poisson-Distribution-II/quiz.py
@@ -1,15 +1,20 @@
-# Question: The manager of a industrial plant is planning to buy a machine of
+# Question: The manager of an industrial plant is planning to buy a machine of
 # either type A or type B. For each day’s operation:
 # The number of repairs, X, that machine A needs is a Poisson random variable
-# with mean 0.88. The daily cost of operating A is C = 160 + 40X².
+# with a mean of 0.88. The daily cost of operating A is C = 160 + 40X².
 # The number of repairs, Y, that machine B needs is a Poisson random variable
-# with mean 1.55. The daily cost of operating B is C = 128 + 40Y².
+# with a mean of 1.55. The daily cost of operating B is C = 128 + 40Y².
+
+# Define functions
+def exp_val(lmda):
+    return (lmda + (lmda**2))
 
 # Set data
-lambdas = list(map(float, input().split()))
-lambda_a = lambdas[0]
-lambda_b = lambdas[1]
+mean = list(map(float,input().split()))
 
-# Gets the result and show on the screen
-print (round(160 + 40 * (lambda_a + lambda_a ** 2), 3))
-print (round(128 + 40 * (lambda_b + lambda_b ** 2), 3))
+# Gets the result and show it on the screen
+cost_a = 160 + (40*exp_val(mean[0]))
+cost_b = 128 + (40*exp_val(mean[1]))
+
+print("{:.3f}".format(cost_a))
+print("{:.3f}".format(cost_b))
