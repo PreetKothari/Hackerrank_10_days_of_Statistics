6ab1b53f2576a843706c26a69d402b2fde3d01b8
Preet Kothari <preetkothari19@gmail.com>
2023-12-26 13:19:06 +0530

    Update quiz.py

--- Day-4/Geometric-Distribution-II/quiz.py
+++ Day-4/Geometric-Distribution-II/quiz.py
@@ -8,11 +8,10 @@ p = probability[0] / probability[1]
 q = 1 - p
 n = int(input())
 
-# Get Geometric Distribution
-result = 0
-for i in range(n + 1):
-    if i > 0:
-        result = result + (q ** (i - 1) * p)
+# Get Geometric Distribution, g(n,p) = (q**(n-1))*p
+g = 0
+for i in range(1, n + 1):
+    g += (q**(i - 1)) * p
 
 # Show the final result
-print(round(result, 3))
+print("{:.3f}".format(g))
