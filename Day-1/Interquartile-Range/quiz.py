30eb5abf6765937d6ed2d9a72bc087499c980a13
Preet Kothari <preetkothari19@gmail.com>
2023-12-26 12:57:31 +0530

    Update quiz.py

--- Day-1/Interquartile-Range/quiz.py
+++ Day-1/Interquartile-Range/quiz.py
@@ -1,32 +1,29 @@
 # Define functions
-def median(size, values):
-    if size % 2 == 0:
-        median = (values[int(size/2)-1] + values[int(size/2)]) / 2
+def median(values,size):
+    if size%2 == 0:
+        median = (values[(size//2)-1]+values[size//2])/2
     else:
-        median = float(values[int(size/2)])
-    return median
+        median = (values[size//2])
+    return int(median)
 
 # Set the data
-size = int(input())
-elements = list(map(int, input().split()))
-frequencies = list(map(int, input().split()))
+n = int(input())
+values = list(map(int,input().split()))
+freqs = list(map(int,input().split()))
 
-new_data = []
-for i in range(len(elements)):
-    for j in range(frequencies[i]):
-        new_data.append(elements[i])
-new_data = sorted(new_data)
+m = []
+for i in range(n):
+    m+= [values[i]]*freqs[i]
+m.sort()
+n = len(m)
 
-# Verify that the total data is even or odd
-size = int(len(new_data))
-if size % 2 == 0:
-    data_low = new_data[0:int(size/2)]
-    data_high = new_data[int(size/2):size]
+# Verify that the total data is even or odd and calculate the quartiles
+if n%2 == 0:
+    q1 = median(m[:(n//2)],(n//2))
+    q3 = median(m[(n//2):],(n//2))
 else:
-    data_low = new_data[0:int(size/2)]
-    data_high = new_data[int(size/2)+1:size]
+    q1 = median(m[:(n//2)],(n//2))
+    q3 = median(m[(n//2)+1:],(n//2))
 
-# Get the final result and print on the screen
-low = median(len(data_low), data_low)
-high = median(len(data_high), data_high)
-print(high - low)
+# Get the final result and print it on the screen
+print("{:.1f}".format(q3-q1))
