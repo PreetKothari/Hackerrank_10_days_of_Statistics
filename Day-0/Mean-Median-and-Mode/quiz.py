1d9a86eaf896f9ad0ef11cfb96e1ad17193a2ec5
Preet Kothari <preetkothari19@gmail.com>
2023-12-26 12:45:46 +0530

    Update quiz.py

--- Day-0/Mean-Median-and-Mode/quiz.py
+++ Day-0/Mean-Median-and-Mode/quiz.py
@@ -1,8 +1,30 @@
-import numpy as np
-from scipy import stats
+n = int(input())
+m = list(map(int,input().split()))
+m.sort()
 
-size = int(input())
-numbers = list(map(int, input().split()))
-print(np.mean(numbers))
-print(np.median(numbers))
-print(int(stats.mode(numbers)[0]))
+mean = (sum(m)/n)
+if n%2 == 0:
+    median = ((m[(n//2)-1] + m[(n//2)])/2)
+else:
+    median = m[n//2]
+
+count = 0
+max = 0
+num = 0
+
+for i in m:
+    if i == num:
+        count +=1
+    else:
+        num = i
+        count = 1
+    if count > max:
+        max = count
+        mode = num
+
+if max < 2:
+    mode = m[0]
+
+print('{:.1f}'.format(mean))
+print('{:.1f}'.format(median))
+print(mode)
