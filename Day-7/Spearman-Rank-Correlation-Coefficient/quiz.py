a08a0fc35b5f31fccc705e4f872bc13dc3625696
Preet Kothari <preetkothari19@gmail.com>
2023-12-26 13:50:49 +0530

    Update quiz.py

--- Day-7/Spearman-Rank-Correlation-Coefficient/quiz.py
+++ Day-7/Spearman-Rank-Correlation-Coefficient/quiz.py
@@ -1,34 +1,28 @@
-# Spearman's Rank Correlation Coefficient
-# Reference: https://en.wikipedia.org/wiki/Spearman%27s_rank_correlation_coefficient
 # Quiz: Given two n-element data sets, X and Y, calculate the value of
 # Spearman's rank correlation coefficient.
 
 # Define functions
-def rank(dt):
-    rank = {}
-    sort = sorted(dt)
-    for i in range(len(dt)):
-        for j in range(len(sort)):
-            if dt[i] == sort[j]:
-                rank[dt[i]] = j + 1
-    return rank
-
-def spearman(x, y, rx, ry, n):
-    diff_rank = 0
+def rank(x):
+    x_sorted = sorted(x)
+    rx = dict()
     for i in range(n):
-        if rx[x[i]] != ry[y[i]]:
-            diff_rank = diff_rank + ((rx[x[i]] - ry[y[i]]) ** 2)
-    return (6 * diff_rank) / (n ** 3 - n)
+        for j in range(n):
+            if x[i] == x_sorted[j]:
+                rx[x[i]] = j + 1
+    return rx
+
+def spearmans_rank_corr_coef(x, y, n):
+    diff_rank = 0
+    rx = rank(x)
+    ry = rank(y)
+    for i in range(n):
+        diff_rank += ((rx[x[i]] - ry[y[i]]) ** 2)
+    return (1 - ((6 * diff_rank)/((n ** 3) - n)))
 
 # Set data
 n = int(float(input()))
-data_set_x = list(map(float, input().split()))
-data_set_y = list(map(float, input().split()))
+x = list(map(float, input().split()))
+y = list(map(float, input().split()))
 
-# Get rank
-rank_x = rank(data_set_x)
-rank_y = rank(data_set_y)
-
-# Gets the result and show on the screen
-s = spearman(data_set_x, data_set_y, rank_x, rank_y, n)
-print(round(1 - s, 3))
+# Gets the result and show it on the screen
+print("{:.3f}".format(spearmans_rank_corr_coef(x, y, n)))
